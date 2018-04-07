57372e404b92444e2a9b980f483102f2b2de96bb
shakedzrihen <shaked.zrihen@gmail.com>
2018-04-07 15:54:49 +0300

    Fix onClickOperator() function

--- app/src/main/java/com/shenkar/shakedzrihen/mycalc/MainActivity.java
+++ app/src/main/java/com/shenkar/shakedzrihen/mycalc/MainActivity.java
@@ -1,15 +1,15 @@
 package com.shenkar.shakedzrihen.mycalc;
 
-import android.support.v7.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.view.View;
 
 import java.text.DecimalFormat;
 import java.util.regex.Pattern;
-import android.util.Log;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,17 +41,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickOperator(View v){
-        if(!"".equals(_result))
+        if("".equals(display)){
+            return;
+        }
+        if(!"".equals(_result))  // if there is a result
         {
-            if("Invalid operation".equals(_result)){
+            if("Invalid Operation".equals(_result)){  // if in the result there is an exception - clear
                 display = "";
                 _result = "";
-            } else {
+                updateScreen();
+                return;
+            } else {  // show the result and clear the old result
                 display = _result;
                 _result = "";
             }
         }
         if(!"".equals(currentOperator)){
+            String[] operation = display.split(Pattern.quote(currentOperator));
+            if(operation.length < 2){
+                Button operator = (Button) v;
+                display = operation[0] + operator.getText();
+                currentOperator = operator.getText().toString();
+                updateScreen();
+                return;
+            }
             calculate("");
             updateScreen();
             _result = "";
@@ -94,7 +107,7 @@ public class MainActivity extends AppCompatActivity {
                     return answerToString(Double.valueOf(x) / Double.valueOf(y));
                 } catch (Exception e) {
                     Log.d("calc", e.getMessage());
-                    return  x;
+                    return  "Invalid Operation";
                 }
             case "x": return answerToString(Double.valueOf(x) * Double.valueOf(y));
             default: return "Invalid Operation";
@@ -115,7 +128,7 @@ public class MainActivity extends AppCompatActivity {
         if ("Invalid Operation".equals(_result)){
             text = _result;
         } else {
-            text = display + "\n" + _result;
+            text = display + "\n=" + _result;
         }
         if("Eq".equals(op)){
             _screen.setText(text);
@@ -126,6 +139,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickEqual(View v){
-        calculate("Eq");
+          calculate("Eq");
     }
 }
